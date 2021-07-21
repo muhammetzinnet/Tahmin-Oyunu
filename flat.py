aaf690a1783e898bc2d686b22f8806fc30693eb9
Mahmut Muhammet <46005482+muhammetzinnet@users.noreply.github.com>
2021-07-21 14:31:51 +0300

    Initial commit

---
+++ flat.py
@@ -0,0 +1,21 @@
+
+
+import numpy as np
+
+
+
+gfg = np.array([[6, 9], [8, 5], [18, 21]])
+
+gfg.flatten()
+
+print(gfg.flatten())
+
+
+
+
+def Ters(lst): 
+    lst.reverse() 
+    return lst 
+      
+lst = [[7, 6, 5], [4, 3], [2, 1]] 
+print(Ters(lst))
